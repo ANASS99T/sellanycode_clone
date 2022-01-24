a7f7b35d7b6422dcdd30913c76c981f09911a1f9
ANASS OULED BEN TAHAR <Anass.taher@gmail.com>
2022-01-24 16:00:38 +0100

    end navbar

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../assets/logo-sellanycode.png"
 import MenuIcon from '@mui/icons-material/Menu';
+import { NavDropdown } from 'react-bootstrap';
 
 export default function Navigation() {
   return (
@@ -33,21 +34,55 @@ export default function Navigation() {
               <Link to="/category" className="text-secondary" style={{ fontWeight: 300, fontSize: "19px" }}>Scripte & Codes</Link>
               <div className="sub-menu d-flex justify-content-around flex-column">
                 <div className="my-1 mx-1 item">
-                  <Link to="/category/ios" className="text-secondary">iOS</Link>
+                  <Link to="/subcat/javaScript" className="text-secondary">javaScript</Link>
                 </div>
                 <div className="my-1 mx-1 item">
-                  <Link to="/category/android" className="text-secondary">Android</Link>
+                  <Link to="/subcat/php" className="text-secondary">Php</Link>
                 </div>
                 <div className="my-1 mx-1 item">
-                  <Link to="/category/unity" className="text-secondary">Unity</Link>
+                  <Link to="/subcat/python" className="text-secondary">Python</Link>
+                </div>
+                <div className="my-1 mx-1 item">
+                  <Link to="/subcat/java" className="text-secondary">Java</Link>
                 </div>
               </div>
             </li>
-            <li className=" menu-item text-secondary" style={{ fontWeight: 300, fontSize: "19px" }}>
+            <li className=" menu-item text-secondary" style={{ position: "relative" }}>
 
               <Link to="/category" className="text-secondary" style={{ fontWeight: 300, fontSize: "19px" }}>Upload your code</Link>
+              <span className="nav-sub-soon" style={{ top: "0px", right: "10px", position: "absolute", textTransform: "uppercase", fontWeight: "700", fontSize: "11px", color: "#6315b8", letterSpacing: 0, opacity: 0.8 }}>SELL!</span>
 
             </li>
+          </ul>
+        </div>
+
+        <div className="connection d-flex align-items-center justify-content-center d-none">
+          <Link to="/login" className="text-secondary">
+            <button className="btn login-button text-uppercase mx-1">
+              Login
+            </button>
+          </Link>
+          <Link to="/signup" className="text-secondary">
+            <button className="btn login-button text-uppercase ">
+              sign up
+            </button>
+          </Link>
+        </div>
+
+        <div className="dropdown">
+          <Link className="btn dropdown-toggle text-secondary" to="/" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+            Hi, Obito <span className="badge bg-primary">$ 0.00</span>
+          </Link>
+
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <li><Link className="dropdown-item my-1 text-secondary" to="/"><i className="far fa-address-card me-2"></i>Dashbaord</Link></li>
+            <li><Link className="dropdown-item my-1 text-secondary" to="/"><i className="fas fa-cloud-download-alt me-2"></i>Downloads</Link></li>
+            <NavDropdown.Divider />
+            <li><Link className="dropdown-item my-1 text-secondary" to="/"><i className="fas fa-th-list me-2"></i>My Items</Link></li>
+            <li><Link className="dropdown-item my-1 text-secondary" to="/"><i className="fas fa-cloud-upload-alt me-2"></i>Sell Items</Link></li>
+            <NavDropdown.Divider />
+            <li><Link className="dropdown-item my-1 text-secondary" to="/"><i className="fas fa-sign-out-alt me-2"></i>Log out</Link></li>
+
           </ul>
         </div>
       </div>
