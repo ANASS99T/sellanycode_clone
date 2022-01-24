41060d7d4887953e74eb4b6f7d01044f40512442
ANASS OULED BEN TAHAR <Anass.taher@gmail.com>
2022-01-25 00:47:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
           <Route path='/user' component={User} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
+          
         </Switch>
       </div>
     </Router>
